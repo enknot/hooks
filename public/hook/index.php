a9ed4df9ffad6444bd16f6ad8b9ee18516f6fdaa
Enknot Muja <enknot@gmail.com>
2016-02-29 17:06:54 -0500

    Update index.php
    Options are from objects from gitlab. Need to test for objects returned from bitbucket, but should only allow updates when master is updated.

--- public/hook/index.php
+++ public/hook/index.php
@@ -1,76 +1,91 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-//includes
 include_once('../../private/config/include.php');
-include_once('../../private/config/wp.php');
+include_once('../../private/config/plain.php');
 
+$use_repo = true;
+$exec = true;
+$log = true;
+
+
+$data['request'] = $_REQUEST;
+$data['server'] = $_SERVER;
+$data['input'] = json_decode(trim(file_get_contents('php://input')));
 
 //config
-if(isset($_REQUEST['project']) &&  array_key_exists($_REQUEST['project'], $config)){
-    extract($config[$_REQUEST['project']]);
-}else{    
-    $data['request'] = $_REQUEST;
-    $data['input'] = json_decode(trim(file_get_contents('php://input')));
-    logger('bad_request', $data);
-    die(print_pre($data));
+if (isset($_REQUEST['project'])) {
+
+    if (array_key_exists($_REQUEST['project'], $config)) {
+        extract($config[$_REQUEST['project']]);
+    } else {
+        badrequest("bad request: invalid project.", $data);
+    }
+} else {
+    $msg = 'bad_request: badly formed request';
+    badrequest($msg, $data);
+    die($msg . '\r' . print_pre($data));
 }
 
-//settings
-$use_repo = true;
-$exec =     false;
-$log =      false;
 
+function gitsite($project, $clone, $repo) {
 
-//variables
-$to_screen['request'] = $_REQUEST;
-$to_screen['git_remote'] = json_decode(trim(file_get_contents('php://input')));
+    global $use_repo;
+    global $exec;
+    global $log;
 
+    $to_screen['git_command'][] = $lab = json_decode(trim(file_get_contents('php://input')));
 
-$ssh = "ssh -p 1027 githook@prod.rosalliance.com ";
-$rsync = '/usr/bin/rsync -avz --delete -e "ssh -p 1027" ';
+    /**
+     * lab pushes only execute when master is branch is updated (and the execute
+     *  option is on already)
+     */
+    if(isset($lab)){
+        $exec = ($lab->ref == 'refs/heads/master')?$exec:false;
+    }
+    
+    $ssh = "ssh -p 1027 githook@prod.rosalliance.com ";
+    $rsync = '/usr/bin/rsync -avz --delete -e "ssh -p 1027" ';
 
-$remote_clone_root = "apache@prod.rosalliance.com:/var/www/html/wordpress/{$project}/wp-content";
-$local_clone_root = "/var/www/html/alliance/private/clones";
+    $web_root = "/var/www/html/plain";
+    $remote_clone_root = "apache@prod.rosalliance.com:/var/www/html/plain";
+    $local_clone_root = "/var/www/html/alliance/private/clones";
 
-$local_clone = "{$local_clone_root}/{$clone}";
-$remote_clone = "{$remote_clone_root}";
+    $local_clone = "{$local_clone_root}/{$clone}";
+    $remote_clone = "{$remote_clone_root}";
+
 
 
 //clone if it doesnt already exist
-$commands = [];
-if ($use_repo) {
-    if (!is_dir($local_clone)) {
-        $commands[] = '/usr/bin/git clone --recursive ' . escapeshellarg($repo) . ' ' . escapeshellarg($local_clone) .' 2>&1';
+    $commands = [];
+    if ($use_repo) {
+        if (!is_dir($local_clone)) {
+            $commands[] = '/usr/bin/git clone ' . escapeshellarg($repo) . ' ' . escapeshellarg($local_clone) . ' 2>&1';
 //otherwise pull origin master branch
-    } else {
-        $commands[] = 'cd ' . escapeshellarg($local_clone) . ';/usr/bin/git pull origin master 2>&1';
+        } else {
+            $commands[] = 'cd ' . escapeshellarg($local_clone) . ';/usr/bin/git pull origin master 2>&1';
+        }
     }
+
+
+    $commands = array_merge($commands, [
+        $rsync . escapeshellarg("{$local_clone}") . ' ' . escapeshellarg("{$remote_clone}")
+    ]);
+
+
+    foreach ($commands as $i => $command) {
+        $output = [];
+        $to_screen['command'][$i] = $command;
+        if ($exec) {
+            exec($command, $output);
+            $to_screen[$i]['output'] = $output;
+        }
+    }
+
+
+    if ($log)
+        logger($project, $to_screen);
+    echo print_pre($to_screen);
 }
 
-
-
-$commands = array_merge($commands, [
-    $rsync . escapeshellarg("{$local_clone}") . ' ' . escapeshellarg("{$remote_clone}"),            
-        ]);
-
-foreach ($commands as $i => $command) {
-    $output = [];
-    $to_screen['command'][$i] = $command;
-
-    if ($exec) {
-        exec($command, $output);
-        $to_screen[$i]['output'] = $output;
-    }
-}
-
-
-if($log)logger($project, $to_screen);
-echo print_pre($to_screen);
-
+//go!
+gitsite($project, $clone, $repo);
